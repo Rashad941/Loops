093cab707f68ca6f7a7ebfacba1b872027286f11
Michael Ethan Rashad-Nakpan <methan941@gmail.com>
2025-04-11 22:31:52 +0000

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,26 @@
+
+// Test
+
+const guestList = ['Rashmann', 'Kwabena', 'Ishfak'];
+ const guestName = prompt ('What is your name?');
+ if (guestName.includes ('Rashmann')) {
+    alert ('You are welcome')
+
+ } else {
+    alert ('Sorry, try again soon');
+ }
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
